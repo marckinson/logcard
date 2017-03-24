1a8f5d595b176d0cdcd0c5c232faaee944ebd15b
marckinson <j.marckinson@yahoo.fr>
2017-03-24 14:06:44 +0100

    Premier Commit

--- finished/chaincode.go
+++ finished/chaincode.go
@@ -330,13 +330,14 @@ func (t *SimpleChaincode) getPart(stub shim.ChaincodeStubInterface, partId strin
 	fmt.Println("Looking for Part #" + partId);
 
 	//get the part index
-	partId = args [0]
 	pAsBytes, err := stub.GetState(partId)
 	if err != nil {
 		return nil, errors.New("Failed to get Part #" + partId)
 	}
 
-	return pAsBytes, nil	
+	
+	return pAsBytes, nil
+	
 }
 
 // ============================================================================================================================
